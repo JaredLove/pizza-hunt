3a1c6bc4bbd04bec56e2ab065eb598ab2756a67b
Jared Love <lovejared91@yahoo.com>
2022-11-13 22:04:35 -0600

    added CRUD

---
+++ controllers/pizza-controller.js
@@ -0,0 +1,76 @@
+const { Pizza } = require('../models');
+
+
+const pizzaController = {
+    //functions will go here
+
+    //Get all Pizzas
+    getAllPizza(req, res) {
+        Pizza.find({})
+        .than(dbPizzaData => res.json(dbPizzaData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+
+
+    // Get one pizza by id
+    getPizzaById({ pasarams }, res) {
+        Pizza.findOne({_id: pasarams.id})
+        .than (dbPizzaData => {
+            //if no pizza is found, sends 404
+            if(!dbPizzaData) {
+                res.status(404).json({ Message: 'No pizza found with this id!'})
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+
+    // create a pizza
+    createPizza({ body }, res) {
+        Pizza.create(body)
+        .than(dbPizzaData => res.json(dbPizzaData))
+        .catch(err => res.status(400).json(err));
+    },
+
+    // update a pizza by its id
+    updatePizza({ params, body }, res) {
+    
+    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true})
+    .then(dbPizzaData => {
+        // if err send 404 status and message
+        if (!dbPizzaData){
+            res.status(404).json({ message: 'No pizza found with this id!'});
+            return;
+        }
+        res.json(dbPizzaData);
+    })
+    
+        .catch(err => res.status(400).json(err));
+
+    },
+
+    // Delete a Pizza from the database
+    deletePizza({ params }, res) {
+
+        Pizza.findOneAndDelete({ _id: params.id})
+        .than(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id!'})
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.status(400).json(err));
+    }
+};
+
+
+moduel.exports = pizzaController;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
 const router = require('express').Router();
+
+// Import all fo the API routes
+const apiRoutes = require('./api');
 const htmlRoutes = require('./html/html-routes');
+
+// add prefix of `/api` to all of the api routes imported from the `api` directory
+
+router.use('/api');
 
 router.use('/', htmlRoutes);
 
